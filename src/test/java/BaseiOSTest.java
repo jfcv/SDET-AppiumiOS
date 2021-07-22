57156e0b011688def341af7a80a0a7b171f869eb
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-22 08:30:40 -0500

    minor adjustments.

--- src/test/java/BaseiOSTest.java
+++ src/test/java/BaseiOSTest.java
@@ -14,18 +14,9 @@ public class BaseiOSTest {
 
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"14.4");
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 12 Pro");
-        /*
-         * XCUI TEST framework used by appium for automating apps on iOS
-         */
-        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
-        /*
-         * waits for the app before starting the test
-         */
-        caps.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT, 50000);
-        /*
-         * waits at each command execution
-         */
-        caps.setCapability("commandTimeouts", "12000");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST); //XCUI TEST framework used by appium for automating apps on iOS
+        caps.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT, 50000); //waits for the app before starting the test
+        caps.setCapability("commandTimeouts", "12000"); //waits at each command execution
         caps.setCapability(MobileCapabilityType.APP, "/Users/juanfernando.cuadros/code/AppiumiOS/app/UIKitCatalog.app");
 
         IOSDriver driver = new IOSDriver<>(new URL("http://localhost:4723/wd/hub"), caps);
