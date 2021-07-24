31390d31b48bfe7f98261668029fe5c14af0498e
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-24 15:16:05 -0500

    [ScrollTest] : Dropdowns on iOS.

--- src/test/java/ScrollTest.java
+++ src/test/java/ScrollTest.java
@@ -4,7 +4,7 @@ import java.net.MalformedURLException;
 import java.util.HashMap;
 
 public class ScrollTest extends BaseiOSTest {
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
         IOSDriver driver = Capabilities();
         //scroll using Javascript
         HashMap<String, Object> scrollObject = new HashMap<>();
@@ -17,5 +17,18 @@ public class ScrollTest extends BaseiOSTest {
         scrollObject.put("label", "Web View");
         driver.executeScript("mobile:scroll", scrollObject);
         driver.findElementByAccessibilityId("Web View").click();
+        /*
+         * must wait right after changing to the webview
+         * so It can find the element
+         */
+        int SECONDS = 5;
+        int MILISECS = 1000;
+        Thread.sleep(SECONDS*MILISECS);
+        driver.findElementByIosClassChain("**/XCUIElementTypeButton[`label == \"UIKitCatalog\"`]").click();
+        driver.findElementByAccessibilityId("Picker View").click();
+        driver.findElementByAccessibilityId("Red color component value").sendKeys("80");
+        driver.findElementByAccessibilityId("Green color component value").sendKeys("220");
+        driver.findElementByAccessibilityId("Blue color component value").sendKeys("105");
+        System.out.println(driver.findElementByAccessibilityId("Blue color component value").getText());
     }
 }
